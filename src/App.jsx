dbc76da9b18d659a35403b71226aaf2e66de3dd3
Renato Sena <158511024+RenatoS1@users.noreply.github.com>
2025-06-11 15:12:52 -0300

    Dando os primeiros passos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css'
+
+import "./App.css";
+import Header from "./components/Header";
+
 
 function App() {
-
-
   return (
     <>
-    <h1 className='bg-red-500'>Renato</h1>
+      <div className="relative z-0 text-white">
+        <main className="relative z-10 p-10">
+          <h1 className="text-4xl font-bold">Meu Portfólio</h1>
+          <p className="mt-4">Bem-vindo ao meu site!</p>
+        </main>
+      </div>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+// src/components/Header.jsx
+
+function Header() {
+  return (
+    <>
+      <h1>Renato</h1>
+      <nav>
+        <a href="http://"></a>
+        <a href="http://"></a>
+        <a href="http://"></a>
+        <a href="http://"></a>
+      </nav>
+    
+    
+    </>
+  );
+}
+
+export default Header;
