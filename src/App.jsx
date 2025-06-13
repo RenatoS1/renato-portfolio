0ef89af1d0b7af8d6f6d43d15622f5e8f8538bcd
Renato Sena <158511024+RenatoS1@users.noreply.github.com>
2025-06-13 16:39:26 -0300

    Procurandos elementos prontos

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,6 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <div className="relative z-0 text-white">
-        <main className="relative z-10 p-10">
-          <h1 className="text-4xl font-bold">Meu Portfólio</h1>
-          <p className="mt-4">Bem-vindo ao meu site!</p>
-        </main>
-      </div>
       <Header />
     </>
   );
